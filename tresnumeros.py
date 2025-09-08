c3a2f5636192265a17cca0f2c95be663f242c7ec
Viviana González <fridavivianabecerragonzalez@gmail.com>
2025-09-07 19:10:45 -0600

    Add files via upload
    El programa pide 3 números y te indica cual es el mayor y cual es el menor

---
+++ tresnumeros.py
@@ -0,0 +1,9 @@
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+mayor = max(num1, num2, num3)
+menor = min(num1, num2, num3)
+
+print(f"El número mayor es: {mayor}")
+print(f"El número menor es: {menor}")
