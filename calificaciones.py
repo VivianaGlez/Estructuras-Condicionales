ec976e3d3618d5988112464822cde2d5c51f9b14
Viviana González <fridavivianabecerragonzalez@gmail.com>
2025-09-07 19:07:40 -0600

    Add files via upload
    te indica la letra que corresponde conforme a la calificación

---
+++ calificaciones.py
@@ -0,0 +1,14 @@
+calificacion = int(input("Ingresa la calificación (0–100): "))
+
+if 90 <= calificacion <= 100:
+    print("Calificación: A")
+elif 80 <= calificacion <= 89:
+    print("Calificación: B")
+elif 70 <= calificacion <= 79:
+    print("Calificación: C")
+elif 60 <= calificacion <= 69:
+    print("Calificación: D")
+elif 0 <= calificacion < 60:
+    print("Calificación: F")
+else:
+    print("Calificación inválida.")
